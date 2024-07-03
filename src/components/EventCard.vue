40fbc84266dc052303fbeb2e0926a8665ea3b824
Aye Nyein San <ayenyeinsan@Ayes-MacBook-Air-2.local>
2024-07-03 23:43:11 +0700

    5,6,7

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,14 +1,44 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
 // defineProps<{
 //   msg: string
 // }>()
+const event = ref({
+    id: 5928101,
+    category: 'animal welfare',
+    title: 'Cat Adoption Day',
+    description:'Find your new feline friend at this event!',
+    location: 'Meow Town',
+    date: 'Jaunary 28,2022',
+    time: '12:00',
+    petsAllowed: true,
+    organizer:'Kat Laydee'
+})
 </script>
 
 <template>
 
      
       
-<div class="greetings"></div>
-</template>
 
+    <div class="event-class">
+        <h2>{{ event.title }}</h2>
+        <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
+
+</template>
+<style scoped>
+.event-class{
+padding: 20px;
+width: 250px;
+cursor: pointer;
+border: 1px solid #39495c;
+margin-bottom: 18px;
+}
+.event-class:hover{
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0,0,0.2);
+}
+</style>
 
