367ddbaec34ce4ba2968476545f8277c64d5b353
Aye Nyein San <ayenyeinsan@Ayes-MacBook-Air-2.local>
2024-07-24 18:12:24 +0700

    till before 2.10

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -7,6 +7,11 @@ import { ref, onMounted,computed,defineProps, watchEffect } from 'vue';
 import EventService from '@/services/EventService';
 
 const events=ref<Event[]| null>(null)
+const totalEvents = ref(0)
+const hasNextPage = computed(()=>{
+  const totalPages = Math.ceil(totalEvents.value/2)
+  return page.value < totalPages
+})
 const props = defineProps({
  page:{
   type: Number,
@@ -19,6 +24,7 @@ onMounted(()=>{
     EventService.getAPIEvents(2, page.value)
   .then((response)=>{
     events.value=  response.data
+    totalEvents.value = response.headers['x-total-count']
   })
   })
 
@@ -35,24 +41,49 @@ onMounted(()=>{
   <!-- <NewEventCard  v-for ="event in events" :key="event.id" :event="event" />> -->
 
 </div>
+<div class = "pagination">
+
+
 <RouterLink 
+id="page-prev"
 :to="{name:'event-list-view',query:{page: page-1}}"
 rel="prev"
 v-if="page != 1"
 
 >
-Prev Page
+ &#60; Prev Page
 </RouterLink>
 <RouterLink
+id="page-next"
 :to="{name:'event-list-view',query:{page: page +1}}"
-rel="next">
-Next Page
+rel="next"
+v-if="hasNextPage">
+Next Page &#62;
 </RouterLink>
+</div>
 </template>
+
 <style scoped>
 .events{
   display:flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination{
+  display: flex;
+  justify-content: center;
+  width: 290px;
+}
+.pagonation a{
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50
+}
+#page-prev{
+  text-align : left;
+
+}
+#page-next{
+  text-align: right;
 }
 </style>
