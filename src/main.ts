9640c675628389d6a0d00216c877f446dd1999c8
aye-nyeinSan <aye2904@gmail.com>
2024-08-06 18:19:16 +0700

    add insight vercel

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import App from './App.vue'
 import router from './router'
 import 'nprogress/nprogress.css'
 import './assets/style.css'
+import { inject } from '@vercel/analytics'
+inject()
 
 const app = createApp(App)
 
